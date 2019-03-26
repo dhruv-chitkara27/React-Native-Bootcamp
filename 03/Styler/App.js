981fd33bcd3656fab1c76acb4a080ebf715c40b0
Dhruv <dhruvchitkara27@gmail.com>
2019-03-27 04:22:39 +0530

    FlexBox

--- 03/Styler/App.js
+++ 03/Styler/App.js
@@ -4,20 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <View style={{width: 50, height: 50, backgroundColor: "#2980b9"}}></View>
-        <View style={{width: 100, height: 100, backgroundColor: "#27ae60"}}></View>
-        <View style={{width: 150, height: 150, backgroundColor: "#f0932b"}}></View>
+      <View style={{flex: 1, flexDirection: "row", justifyContent: "space-between", marginTop: 50 }}>
+        <View style={{ width: 100, height: 100, backgroundColor: "#2980b9"}} />
+        <View style={{ width: 100, height: 100, backgroundColor: "#27ae60"}} />
+        <View style={{ width: 100, height: 100, backgroundColor: "#f0932b"}} />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
