b1c09a81cb8c5c19c2a786d3b2188c0a3eeec34f
Dhruv <dhruvchitkara27@gmail.com>
2019-03-27 05:20:29 +0530

    TextInput and change state

--- 03/StateIntro/StateIntro/App.js
+++ 03/StateIntro/StateIntro/App.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       text: ''
-    }
+    };
   }
   
   
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <TextInput
+        style={styles.input}
+        placeholderTextColor="#7B8788"
+        placeholder="Enter value here..."
+        onChangeText={ text => {
+          this.setState({ text }) 
+        }}
+        />
+        <Text style={{
+          marginTop: 60,
+          fontSize: 25
+        }}
+        >{this.state.text
+          .split(' ')
+            .map(text => text && "🙃")
+            .join(' ')
+        }</Text>
       </View>
     );
   }
@@ -24,6 +40,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  input: {
+    marginTop: 40,
+    height: 50,
+    width: 200,
+    borderWidth: 2,
+    padding: 10,
+    borderRadius: 5,
+    borderColor: "#23abab"
+  }
 });
